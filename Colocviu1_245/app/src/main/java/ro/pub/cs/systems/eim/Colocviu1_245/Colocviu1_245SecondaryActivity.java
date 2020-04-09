a2c5cab845f9cefbe23d176698c2a32c0c2cc5b7
Cristina Ivan <mcristina.ivan@gmail.com>
2020-04-09 14:53:43 +0300

    implemented B

--- Colocviu1_245/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245SecondaryActivity.java
+++ Colocviu1_245/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245SecondaryActivity.java
@@ -1,7 +1,6 @@
 package ro.pub.cs.systems.eim.Colocviu1_245;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,7 +10,6 @@ public class Colocviu1_245SecondaryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_245_main);
         Intent intentFromMain= getIntent();
-        Bundle data = intentFromMain.getExtras();
 
         String sum = intentFromMain.getStringExtra("ALL TERMS");
         String[] numbers = sum.replaceAll("\\s","").split("\\+", 10);
