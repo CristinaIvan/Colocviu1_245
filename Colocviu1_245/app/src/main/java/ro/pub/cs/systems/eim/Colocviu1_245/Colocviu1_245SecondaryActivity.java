8ea95f84dcd64ff9c279c2e2568dce53ee3b9415
Cristina Ivan <mcristina.ivan@gmail.com>
2020-04-09 14:46:10 +0300

    implemented B

--- Colocviu1_245/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245SecondaryActivity.java
+++ Colocviu1_245/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245SecondaryActivity.java
@@ -10,12 +10,10 @@ public class Colocviu1_245SecondaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_245_main);
-        // intent from parent
-        Intent intentFromParent = getIntent();
-        Bundle data = intentFromParent.getExtras();
+        Intent intentFromMain= getIntent();
+        Bundle data = intentFromMain.getExtras();
 
-        // process information from data ...
-        String sum = intentFromParent.getStringExtra("ALL TERMS");
+        String sum = intentFromMain.getStringExtra("ALL TERMS");
         String[] numbers = sum.replaceAll("\\s","").split("\\+", 10);
 
         int finalSum = 0;
@@ -23,7 +21,6 @@ public class Colocviu1_245SecondaryActivity extends AppCompatActivity {
             finalSum += Integer.parseInt(num);
         }
 
-        // intent to parent
         Intent intentToParent = new Intent();
         intentToParent.putExtra("Sum", finalSum);
         setResult(RESULT_OK, intentToParent);
